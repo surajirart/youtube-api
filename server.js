0c5de61c04077a8a3e26663f30459e2de31b5a26
surajirart <146520537+surajirart@users.noreply.github.com>
2026-05-13 13:28:05 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({ message: "API working 🚀" });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server running");
+});
